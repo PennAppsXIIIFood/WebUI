e7724f50eb9e83f9941e414ca8bb65899fc9e5c1
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2016-01-24 00:27:39 -0500

    added images and prep for js events

---
+++ assets/js/images.js
@@ -0,0 +1,22 @@
+/**
+cheating the fact that I don't know how to do backend yet.
+At least not in the time we had available 
+#CarryingTheTeam
+**/
+
+
+    /**
+click the divs where the pins on the map 
+to show what foods are available
+**/
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+
+    
