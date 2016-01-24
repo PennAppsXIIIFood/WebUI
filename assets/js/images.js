8b46d53e7b95b27faa1c4c41b6b57aee41abb438
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2016-01-24 11:31:01 -0500

    adding other map routes and using css to toggle them

--- assets/js/images.js
+++ assets/js/images.js
@@ -11,17 +11,38 @@ to show what foods are available
 **/
 
     
-
-
-
     
-//$(document).ready(function(){
-//    $(".one").click(function(){
-//        $(".fruit", ".vegetables").hide();
-//    });
-//    $(".two").click(function(){
-//        $(".fruit").show();
-//    });
-//
-//
-//});
+/*$(document).ready(function(){
+    $(".one").click(function(){
+        $(".fruit", ".vegetables").hide();
+    });
+    $(".two").click(function(){
+        $(".fruit").show();
+    });
+
+
+});*/
+
+
+
+/*
+click to show navigation to food
+*/
+
+$(document).ready(function(){
+    $(".one").click();
+  //  $('#signIn, #signUp, #crowdBox, #search').css('opacity', '0.0');
+    $(window).animate({
+        scrollTop: $(".fbh").offset().top
+    }, 1000);  
+    console.log("done");
+
+
+$('.one').click(function(){    
+            $('.fbh').fadeIn('slow');
+        console.log("sign in");
+    });
+    
+});
+
+
